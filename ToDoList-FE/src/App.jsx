165cba06418913252a48a38832ac65c89955fb69
hhw51 <harishassanwyne@gmail.com>
2024-07-04 15:06:13 +0500

    BACKEND FETCHINGG USERS

---
+++ ToDoList-FE/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Login from './Login'
+import SignUp from './Sign-up';
+import Index from './index';
+  
+function App(){
+
+  const [login, setLogin] = useState(true);
+
+  return (
+    <>
+    {/* {login ? <Login setLogin={setLogin}/> : <SignUp setLogin={setLogin}/>} */}
+    <Index/>    
+
+  </>
+    );
+}
+
+export default App;
